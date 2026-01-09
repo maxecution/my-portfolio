0a84f352be9fc455ce1a09816cd125c65310503b
Max <mzimmer90@gmail.com>
2026-01-09 11:18:43 +0000

    updated contact button to scroll straight to contact form. Added scroll to home arrow to become visible when scrolled past landing page

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import About from '@sections/about/About';
 import Projects from '@sections/projects/Projects';
 import Contact from '@sections/contact/Contact';
 import Footer from '@sections/footer/Footer';
+import Arrow from '@ui/arrow/Arrow';
+import useScrollState from '@hooks/useScrollState';
 
 function App() {
+  const hasScrolled = useScrollState(650);
   return (
     <ThemeProvider>
       <ToasterProvider>
@@ -28,6 +31,15 @@ function App() {
           </Section>
         </main>
         <Footer />
+        {hasScrolled && (
+          <div aria-hidden={hasScrolled}>
+            <a
+              href='#'
+              className='fixed bottom-3 right-3 md:bottom-10 md:right-10 border-2 border-primary/50 bg-background rounded-full p-1 transition-transform hover:-translate-y-1 motion-reduce:transform-none'>
+              <Arrow className='text-primary/80' direction='up' />
+            </a>
+          </div>
+        )}
       </ToasterProvider>
     </ThemeProvider>
   );
--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
         <p className='text-muted-foreground text-lg md:text-xl max-w-2xl mx-auto'>{heroData.text}</p>
         <div className='flex flex-col md:flex-row gap-5'>
           <a
-            href='#contact'
+            href='#contact-form'
             className='border-2 border-primary rounded-lg px-8 py-4 text-primary font-bold transition-transform hover:scale-105 shadow-primary/50 hover:shadow-lg'
             data-testid='contact-link'>
             Get in Touch
