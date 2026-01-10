caa7aaad9bd8a0a4935c9a8dc442fb53920c95e4
Max <mzimmer90@gmail.com>
2026-01-10 11:51:17 +0000

    fixed sidescroll issue introduced by runic background. Instead of clipping overflow in background component itself, put background in relative wrapper

--- src/sections/hero/Hero.spec.tsx
+++ src/sections/hero/Hero.spec.tsx
@@ -98,7 +98,6 @@ describe('Hero Component', () => {
         'absolute',
         'bottom-5',
         'left-1/2',
-        'transform',
         '-translate-x-1/2',
         'transition-all',
         'duration-700',
